d85c32b0de3e1008958d5dec7bb9bb73fe03bc97
LuisSevillano <luissevillanopires@gmail.com>
2024-09-19 00:52:30 +0200

    Basic functionality

--- content.js
+++ content.js
@@ -1,34 +1,33 @@
 const apiKey = "27fef946-b325-46ab-a40c-698b24a9e94a";
-
-// Obtener coordenadas y calcular la distancia
+// Fetch coordinates and calculate the driving distance
 function fetchRouteData(latitude, longitude, callback) {
   console.log("fetchRouteData", { latitude, longitude, callback });
 
-  const start = "40.4168,-3.7038"; // Coordenadas de Madrid
+  const start = "40.4168,-3.7038"; // Madrid coordinates
   const end = `${latitude},${longitude}`;
 
   const routeUrl = `https://graphhopper.com/api/1/route?point=${start}&point=${end}&vehicle=car&key=${apiKey}`;
 
   fetch(routeUrl)
     .then(processRouteResponse)
-    .then(data => calculateTimeAndDistance(data, callback))
+    .then((data) => calculateTimeAndDistance(data, callback))
     .catch(logRouteError);
 }
 
-// Procesar la respuesta de la API de GraphHopper
+// Handle the response from the GraphHopper API
 function processRouteResponse(response) {
   console.log("processRouteResponse");
   console.log(response);
   return response.json();
 }
 
-// Calcular tiempo y distancia
+// Calculate time and distance from the route data
 function calculateTimeAndDistance(data, callback) {
   console.log("calculateTimeAndDistance");
   console.log({ data, callback });
   const path = data.paths[0];
-  const timeInSeconds = path.time / 1000; // Convertir tiempo a segundos
-  const distanceInKm = path.distance / 1000; // Convertir distancia a kilómetros
+  const timeInSeconds = path.time / 1000; // Convert time to seconds
+  const distanceInKm = parseInt(path.distance / 1000); // Convert distance to kilometers
 
   const timeInMinutes = Math.floor(timeInSeconds / 60);
   const hours = Math.floor(timeInMinutes / 60);
@@ -37,57 +36,74 @@ function calculateTimeAndDistance(data, callback) {
   const timeFormatted = formatTime(hours, minutes);
   const color = getTimeColor(hours, minutes);
 
-  callback(distanceInKm.toFixed(2), timeFormatted, color);
+  callback(distanceInKm, timeFormatted, color);
 }
 
-// Formatear tiempo
+// Format the time to display it in hours and minutes
 function formatTime(hours, minutes) {
   console.log("formatTime", { hours, minutes });
-  return hours > 0 ? `${hours}h y ${minutes}m` : `${minutes}m`;
+  return hours > 0 ? `${hours}h and ${minutes}m` : `${minutes}m`;
 }
 
-// Obtener el color según el tiempo
+// Determine the color based on the travel time
 function getTimeColor(hours, minutes) {
   console.log("getTimeColor", { hours, minutes });
   const totalHours = hours + minutes / 60;
   if (totalHours >= 3) {
-    return "red"; // Más de 3 horas
+    return { color: "red", className: "discarded" }; // More than 3 hours
   } else if (totalHours >= 2) {
-    return "orange"; // Entre 2 y 3 horas
+    return { color: "orange", className: "target-orange" }; // Between 2 and 3 hours
   } else {
-    return "green"; // Menos de 2 horas
+    return { color: "green", className: "target-green" }; // Less than 2 hours
   }
 }
 
-function getPopupHTMLTemplate(distance, time, color) {
+function getPopupHTMLTemplate(distance, time, colorData) {
   return `<div>
             <span class="close-btn">×</span>
-            <p>Distancia a Madrid: ${distance} km</p>
-            <p style="color: ${color};">Tiempo en coche: ${time}</p>
+            <p class="">
+                <span class="color-distance" style="color: ${colorData.color};">${time}</span> from Madrid
+                <span class="km-distance"> (${distance}km)</span>
+            </p>
         </div>`;
 }
 
-// Mostrar el popup con los datos de distancia
-function createPopup(distance, time, color) {
+// Create the popup displaying the distance and travel time
+function createPopup(distance, time, color, isList) {
   console.log("createPopup", { distance, time, color });
   const popup = document.createElement("div");
   popup.className = "floating-distance-popup";
+  if (isList === true) {
+    popup.className += " in-list";
+  }
   popup.innerHTML = getPopupHTMLTemplate(distance, time, color);
   return popup;
 }
 
-// Mostrar el popup en cada anuncio del listado
+// Display the popup for each listing item
 function displayPopupInListing(item, latitude, longitude) {
   console.log("displayPopupInListing", { item, latitude, longitude });
-  const popup = createPopup("", "", "green");
-  item.querySelector(".price-row").appendChild(popup);
+  const isList = true;
+
+  // Ensure only one popup is present per listing
+  let existingPopup = item.querySelector(".floating-distance-popup");
+  if (existingPopup) {
+    existingPopup.remove();
+  }
+
+  const popup = createPopup("", "", "green", isList);
+
+  // Insert the popup just above the element with the 'price-row' class
+  const priceRow = item.querySelector(".price-row");
+  priceRow.parentNode.insertBefore(popup, priceRow);
 
   fetchRouteData(latitude, longitude, (distance, time, color) => {
+    item.classList.add(color.className);
     updatePopupContent(popup, distance, time, color);
   });
 }
 
-// Actualizar el contenido del popup
+// Update the content of the popup with the actual data
 function updatePopupContent(popup, distance, time, color) {
   console.log("updatePopupContent", { popup, distance, time, color });
   popup.innerHTML = getPopupHTMLTemplate(distance, time, color);
@@ -96,54 +112,58 @@ function updatePopupContent(popup, distance, time, color) {
     .addEventListener("click", closePopup.bind(null, popup));
 }
 
-// Cerrar el popup
+// Close the popup by removing it from the DOM
 function closePopup(popup) {
   popup.remove();
 }
 
-// Manejar la respuesta del listado de anuncios
-function handleListingItem(entry) {
-  console.log("handleListingItem", { entry });
-  if (entry.isIntersecting) {
-    const item = entry.target;
-    const id = item.getAttribute("data-element-id");
-    if (id) {
-      fetchListingData(id, item);
+// Handle entries for each listing item that is being observed
+function handleListingItem(entries, observer) {
+  console.log("handleListingItem", { entries });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const item = entry.target;
+      const id = item.getAttribute("data-element-id");
+      console.log({ item, id });
+      if (id) {
+        fetchListingData(id, item);
+        observer.unobserve(entry.target); // Stop observing once the popup is loaded
+      }
     }
-  }
+  });
 }
 
-// Obtener los datos de un anuncio desde la API de Idealista
+// Fetch data for a listing from Idealista's API
 function fetchListingData(id, item) {
   console.log("fetchListingData", { id, item });
   const apiUrl = `https://www.idealista.com/es/openDetailGallery/${id}?`;
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       const coordinates = data.data.map.coordinates;
       if (coordinates && coordinates.latitude && coordinates.longitude) {
         displayPopupInListing(
           item,
           coordinates.latitude,
-          coordinates.longitude
+          coordinates.longitude,
         );
       }
     });
 }
 
-// Observar los elementos del listado
+// Observe all listing elements in the page
 function observeListings() {
   console.log("observeListings");
   const observer = new IntersectionObserver(handleListingItem, {
     rootMargin: "0px 0px 500px 0px",
   });
 
-  document.querySelectorAll(".item").forEach(item => {
+  document.querySelectorAll(".item").forEach((item) => {
     observer.observe(item);
   });
 }
 
-// Manejar la vista de detalle del anuncio
+// Handle detailed view for an individual property
 function handleDetailView() {
   console.log("handleDetailView");
   const url = window.location.href;
@@ -154,14 +174,14 @@ function handleDetailView() {
   }
 }
 
-// Obtener los datos del anuncio de la vista de detalle
+// Fetch data for the detailed view of a listing
 function fetchDetailData(propertyId) {
   console.log("fetchDetailData", { propertyId });
   const apiUrl = `https://www.idealista.com/es/openDetailGallery/${propertyId}?`;
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const coordinates = data.data.map.coordinates;
+    .then((response) => response.json())
+    .then((response) => {
+      const { coordinates } = response.data.map;
       if (coordinates && coordinates.latitude && coordinates.longitude) {
         const popup = createPopup("", "", "green");
         document.body.appendChild(popup);
@@ -170,18 +190,18 @@ function fetchDetailData(propertyId) {
           coordinates.longitude,
           (distance, time, color) => {
             updatePopupContent(popup, distance, time, color);
-          }
+          },
         );
       }
     });
 }
 
-// Log de errores al obtener rutas
+// Log any route-related errors
 function logRouteError(error) {
-  console.error("Error al obtener la ruta:", error);
+  console.error("Error fetching the route:", error);
 }
 
-// Detectar el contexto de la página y ejecutar la función adecuada
+// Determine the context of the page and trigger the appropriate functions
 window.addEventListener("load", function () {
   if (window.location.href.includes("/inmueble/")) {
     handleDetailView();
