529b57a90ef26455402d7e0e59f9677e8b2a8bf8
LuisSevillano <luissevillanopires@gmail.com>
2024-09-20 11:18:20 +0200

    Add configuration panel. Add secondary starting point

--- content.js
+++ content.js
@@ -1,32 +1,47 @@
 // Fetch coordinates and calculate the driving distance
-function fetchRouteData(latitude, longitude, callback) {
-  chrome.storage.sync.get(
-    ["apiKey", "latitude", "longitude"],
-    function (items) {
-      const start = `${items.latitude || "40.4168"},${items.longitude || "-3.7038"}`; // Default is Madrid
-      const end = `${latitude},${longitude}`;
-      console.log(items);
-      const routeUrl = `https://graphhopper.com/api/1/route?point=${start}&point=${end}&vehicle=car&key=${items.apiKey}`;
+async function fetchRouteData(latitude, longitude) {
+  const { apiKey, primaryOrigin, secondaryOrigin } =
+    await chrome.storage.sync.get([
+      "apiKey",
+      "primaryOrigin",
+      "secondaryOrigin",
+    ]);
 
-      fetch(routeUrl)
-        .then(processRouteResponse)
-        .then((data) => calculateTimeAndDistance(data, callback))
-        .catch(logRouteError);
-    },
+  const primaryCoordinates = primaryOrigin?.coordinates || "40.4168,-3.7038"; // Default to Madrid
+  const end = `${latitude},${longitude}`;
+
+  const primaryRouteUrl = `https://graphhopper.com/api/1/route?point=${primaryCoordinates}&point=${end}&vehicle=car&key=${apiKey}`;
+  const primaryData = await fetchRoute(primaryRouteUrl);
+  const primaryResult = calculateTimeAndDistance(
+    primaryData,
+    primaryOrigin?.city,
   );
+
+  let secondaryResult = null;
+  if (secondaryOrigin) {
+    const secondaryCoordinates = secondaryOrigin.coordinates;
+    const secondaryRouteUrl = `https://graphhopper.com/api/1/route?point=${secondaryCoordinates}&point=${end}&vehicle=car&key=${apiKey}`;
+    const secondaryData = await fetchRoute(secondaryRouteUrl);
+    secondaryResult = calculateTimeAndDistance(
+      secondaryData,
+      secondaryOrigin.city,
+    );
+  }
+
+  return { primaryResult, secondaryResult };
 }
 
-// Handle the response from the GraphHopper API
-function processRouteResponse(response) {
-  console.log("processRouteResponse");
-  console.log(response);
+// Fetch the route data from GraphHopper API
+async function fetchRoute(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Error fetching the route data");
+  }
   return response.json();
 }
 
 // Calculate time and distance from the route data
-function calculateTimeAndDistance(data, callback) {
-  console.log("calculateTimeAndDistance");
-  console.log({ data, callback });
+function calculateTimeAndDistance(data, locationLabel) {
   const path = data.paths[0];
   const timeInSeconds = path.time / 1000; // Convert time to seconds
   const distanceInKm = parseInt(path.distance / 1000); // Convert distance to kilometers
@@ -38,18 +53,16 @@ function calculateTimeAndDistance(data, callback) {
   const timeFormatted = formatTime(hours, minutes);
   const color = getTimeColor(hours, minutes);
 
-  callback(distanceInKm, timeFormatted, color);
+  return { distanceInKm, timeFormatted, color, locationLabel };
 }
 
 // Format the time to display it in hours and minutes
 function formatTime(hours, minutes) {
-  console.log("formatTime", { hours, minutes });
-  return hours > 0 ? `${hours}h and ${minutes}m` : `${minutes}m`;
+  return hours > 0 ? `${hours}h ${minutes}m` : `${minutes}m`;
 }
 
 // Determine the color based on the travel time
 function getTimeColor(hours, minutes) {
-  console.log("getTimeColor", { hours, minutes });
   const totalHours = hours + minutes / 60;
   if (totalHours >= 3) {
     return { color: "red", className: "discarded" }; // More than 3 hours
@@ -60,6 +73,19 @@ function getTimeColor(hours, minutes) {
   }
 }
 
+// Create the popup displaying the distance and travel time
+function createPopup(isList = false, isSecondaryPopup = false) {
+  const popup = document.createElement("div");
+  popup.className = "floating-distance-popup";
+  if (isSecondaryPopup === true) {
+    popup.classList.add("secondary-popup");
+  }
+  if (isList === true) {
+    popup.className += " in-list";
+  }
+  return popup;
+}
+
 function createDotIndicator(colorData) {
   const div = document.createElement("div");
   div.classList.add("dot-indicator");
@@ -67,56 +93,22 @@ function createDotIndicator(colorData) {
   return div;
 }
 
-function getPopupHTMLTemplate(distance, time, colorData) {
+function getPopupHTMLTemplate(distance, time, colorData, locationLabel) {
   return `<div>
-            <span class="close-btn">×</span>
-            <p class="">
-                <span class="color-distance" style="color: ${colorData.color};">${time}</span> from Madrid
-                <span class="km-distance"> (${distance}km)</span>
-            </p>
-        </div>`;
-}
-
-// Create the popup displaying the distance and travel time
-function createPopup(distance, time, color, isList) {
-  console.log("createPopup", { distance, time, color });
-  const popup = document.createElement("div");
-  popup.className = "floating-distance-popup";
-  if (isList === true) {
-    popup.className += " in-list";
-  }
-  popup.innerHTML = getPopupHTMLTemplate(distance, time, color);
-  return popup;
-}
-
-// Display the popup for each listing item
-function displayPopupInListing(item, latitude, longitude) {
-  console.log("displayPopupInListing", { item, latitude, longitude });
-  const isList = true;
-
-  // Ensure only one popup is present per listing
-  let existingPopup = item.querySelector(".floating-distance-popup");
-  if (existingPopup) {
-    existingPopup.remove();
-  }
-  const popup = createPopup("", "", "green", isList);
-
-  // Insert the popup just above the element with the 'price-row' class
-  const priceRow = item.querySelector(".price-row");
-  priceRow.parentNode.insertBefore(popup, priceRow);
-
-  fetchRouteData(latitude, longitude, (distance, time, color) => {
-    const dotIndicator = createDotIndicator(color);
-    item.classList.add(color.className);
-    item.appendChild(dotIndicator);
-    updatePopupContent(popup, distance, time, color);
-  });
+      <span class="close-btn">×</span>
+      <p class="">
+        <span class="color-distance" style="color: ${colorData.color};">
+          ${time}
+        </span>
+        from ${locationLabel}
+        <span class="km-distance"> (${distance} km)</span>
+      </p>
+    </div>`;
 }
 
 // Update the content of the popup with the actual data
-function updatePopupContent(popup, distance, time, color) {
-  console.log("updatePopupContent", { popup, distance, time, color });
-  popup.innerHTML = getPopupHTMLTemplate(distance, time, color);
+function updatePopupContent(popup, distance, time, color, locationLabel) {
+  popup.innerHTML = getPopupHTMLTemplate(distance, time, color, locationLabel);
   popup
     .querySelector(".close-btn")
     .addEventListener("click", closePopup.bind(null, popup));
@@ -127,14 +119,64 @@ function closePopup(popup) {
   popup.remove();
 }
 
+// Display the popup for each listing item
+async function displayPopupInListing(item, latitude, longitude) {
+  const isList = true;
+  let popupContainer = item.querySelector(".popup-container");
+
+  if (!popupContainer) {
+    popupContainer = document.createElement("div");
+    popupContainer.classList.add("popup-container");
+    const priceRow = item.querySelector(".price-row");
+    priceRow.parentNode.insertBefore(popupContainer, priceRow);
+  }
+
+  const popupPrimary = createPopup(isList);
+  popupContainer.appendChild(popupPrimary);
+
+  try {
+    const { primaryResult, secondaryResult } = await fetchRouteData(
+      latitude,
+      longitude,
+    );
+    const dotIndicator = createDotIndicator(primaryResult.color);
+    item.classList.add(primaryResult.color.className);
+    item.appendChild(dotIndicator);
+
+    // Update popup for primary location
+    updatePopupContent(
+      popupPrimary,
+      primaryResult.distanceInKm,
+      primaryResult.timeFormatted,
+      primaryResult.color,
+      primaryResult.locationLabel,
+    );
+
+    // Handle secondary location popup
+    if (secondaryResult) {
+      const isSecondaryPopup = true;
+      const popupSecondary = createPopup(isList, isSecondaryPopup);
+      popupContainer.appendChild(popupSecondary);
+
+      updatePopupContent(
+        popupSecondary,
+        secondaryResult.distanceInKm,
+        secondaryResult.timeFormatted,
+        secondaryResult.color,
+        secondaryResult.locationLabel,
+      );
+    }
+  } catch (error) {
+    console.error("Error displaying popups:", error);
+  }
+}
+
 // Handle entries for each listing item that is being observed
 function handleListingItem(entries, observer) {
-  console.log("handleListingItem", { entries });
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       const item = entry.target;
       const id = item.getAttribute("data-element-id");
-      console.log({ item, id });
       if (id) {
         fetchListingData(id, item);
         observer.unobserve(entry.target); // Stop observing once the popup is loaded
@@ -144,26 +186,22 @@ function handleListingItem(entries, observer) {
 }
 
 // Fetch data for a listing from Idealista's API
-function fetchListingData(id, item) {
-  console.log("fetchListingData", { id, item });
+async function fetchListingData(id, item) {
   const apiUrl = `https://www.idealista.com/es/openDetailGallery/${id}?`;
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const coordinates = data.data.map.coordinates;
-      if (coordinates && coordinates.latitude && coordinates.longitude) {
-        displayPopupInListing(
-          item,
-          coordinates.latitude,
-          coordinates.longitude,
-        );
-      }
-    });
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const { coordinates } = data.data.map;
+    if (coordinates && coordinates.latitude && coordinates.longitude) {
+      displayPopupInListing(item, coordinates.latitude, coordinates.longitude);
+    }
+  } catch (error) {
+    console.error("Error fetching listing data:", error);
+  }
 }
 
-// Observe all listing elements in the page
+// Observe all listing elements on the page
 function observeListings() {
-  console.log("observeListings");
   const observer = new IntersectionObserver(handleListingItem, {
     rootMargin: "0px 0px 500px 0px",
   });
@@ -174,36 +212,40 @@ function observeListings() {
 }
 
 // Handle detailed view for an individual property
-function handleDetailView() {
-  console.log("handleDetailView");
+async function handleDetailView() {
   const url = window.location.href;
   const match = url.match(/\/inmueble\/(\d+)/);
   if (match) {
     const propertyId = match[1];
-    fetchDetailData(propertyId);
+    await fetchDetailData(propertyId);
   }
 }
 
 // Fetch data for the detailed view of a listing
-function fetchDetailData(propertyId) {
-  console.log("fetchDetailData", { propertyId });
+async function fetchDetailData(propertyId) {
   const apiUrl = `https://www.idealista.com/es/openDetailGallery/${propertyId}?`;
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((response) => {
-      const { coordinates } = response.data.map;
-      if (coordinates && coordinates.latitude && coordinates.longitude) {
-        const popup = createPopup("", "", "green");
-        document.body.appendChild(popup);
-        fetchRouteData(
-          coordinates.latitude,
-          coordinates.longitude,
-          (distance, time, color) => {
-            updatePopupContent(popup, distance, time, color);
-          },
-        );
-      }
-    });
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const { coordinates } = data.data.map;
+    if (coordinates && coordinates.latitude && coordinates.longitude) {
+      const popup = createPopup();
+      document.body.appendChild(popup);
+      const { primaryResult } = await fetchRouteData(
+        coordinates.latitude,
+        coordinates.longitude,
+      );
+      updatePopupContent(
+        popup,
+        primaryResult.distanceInKm,
+        primaryResult.timeFormatted,
+        primaryResult.color,
+        primaryResult.locationLabel,
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching detailed view data:", error);
+  }
 }
 
 // Log any route-related errors
@@ -212,19 +254,18 @@ function logRouteError(error) {
 }
 
 // Determine the context of the page and trigger the appropriate functions
-window.addEventListener("load", function () {
-  chrome.storage.sync.get(["apiKey"], function ({ apiKey }) {
-    if (!apiKey) {
-      console.log(
-        "Please set your GraphHopper API Key in the extension settings.",
-      );
-      return;
-    } else {
-      if (window.location.href.includes("/inmueble/")) {
-        handleDetailView();
-      } else {
-        observeListings();
-      }
-    }
-  });
+window.addEventListener("load", async function () {
+  const { apiKey } = await chrome.storage.sync.get(["apiKey"]);
+  if (!apiKey) {
+    console.log(
+      "Please set your GraphHopper API Key in the extension settings.",
+    );
+    return;
+  }
+
+  if (window.location.href.includes("/inmueble/")) {
+    handleDetailView();
+  } else {
+    observeListings();
+  }
 });
--- options.js
+++ options.js
@@ -1,19 +1,47 @@
 // Save settings
 
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.storage.sync.get(["apiKey", "originCoordinates"], function (data) {
-    document.getElementById("api-key").value = data.apiKey || "";
-    document.getElementById("coordinates").value = data.originCoordinates || "";
-    document.getElementById("coordinates-secondary").value =
-      data.originCoordinatesSecondary || "";
-  });
+  chrome.storage.sync.get(
+    ["apiKey", "primaryOrigin", "secondaryOrigin"],
+    function (data) {
+      if (Object.keys(data).length !== 0) {
+        document.getElementById("api-key").value = data.apiKey || "";
+        document.getElementById("coordinates").value =
+          data.primaryOrigin.coordinates || "";
+        addButtonActiveClassFromStorage("primary", data.primaryOrigin.city);
+
+        if (data.secondaryOrigin !== undefined) {
+          document.getElementById("coordinates-secondary").value =
+            data.secondaryOrigin.coordinates || "";
+          addButtonActiveClassFromStorage(
+            "secondary",
+            data.secondaryOrigin.city,
+          );
+        }
+      }
+    },
+  );
 });
+
+function addButtonActiveClassFromStorage(group, cityLabel) {
+  document
+    .querySelectorAll(`.origin-options.${group} button`)
+    .forEach((button) => {
+      if (button.dataset.city === cityLabel) {
+        button.classList.add("active");
+      }
+    });
+}
 
 document.getElementById("saveOptions").addEventListener("click", function () {
   const apiKey = document.getElementById("api-key").value;
   const originCoordinates = document.getElementById("coordinates").value;
+  const originCoordinatesCity = document.querySelector(
+    ".origin-options.primary button.active",
+  ).dataset.city;
+
   const originCoordinatesSecondary = document.getElementById(
-    "coordinates-secondary"
+    "coordinates-secondary",
   ).value;
 
   if (!apiKey) {
@@ -21,12 +49,28 @@ document.getElementById("saveOptions").addEventListener("click", function () {
     return;
   }
 
-  let data = { apiKey, originCoordinates };
+  let data = {
+    apiKey,
+    primaryOrigin: {
+      coordinates: originCoordinates,
+      city: originCoordinatesCity,
+    },
+  };
 
   if (originCoordinatesSecondary !== "") {
-    data.originCoordinatesSecondary = originCoordinatesSecondary;
-  }
+    const originCoordinatesSecondaryCity = document.querySelector(
+      ".origin-options.secondary button.active",
+    ).dataset.city;
 
+    data.secondaryOrigin = {
+      coordinates: originCoordinatesSecondary,
+      city: originCoordinatesSecondaryCity,
+    };
+  } else {
+    chrome.storage.sync.remove("originCoordinatesSecondary", function () {
+      // console.log("Secondary coordinates removed from storage.");
+    });
+  }
   chrome.storage.sync.set(data, function () {
     alert("Options saved successfully!");
   });
@@ -48,27 +92,39 @@ window.addEventListener("load", function () {
 });
 
 const citiesCoordinates = {
-  madrid: [40.4168, -3.7038],
-  aranda: [41.668602, -3.6953297],
-  zaragoza: [41.654123, -0.879338],
-  donosti: [43.3152668, -2.0029028],
-  lisboa: [38.723263, -9.130241],
+  Madrid: [40.4168, -3.7038],
+  Aranda: [41.668602, -3.6953297],
+  Zaragoza: [41.654123, -0.879338],
+  Donosti: [43.3152668, -2.0029028],
+  Lisboa: [38.723263, -9.130241],
 };
 
-document.querySelectorAll(".origin-options.primary button").forEach(button => {
-  const city = button.getAttribute("data-city");
-  button.addEventListener("click", function (e) {
-    const coordinates = citiesCoordinates[city]; // Madrid coordinates
-    document.getElementById("coordinates").value = coordinates.join(",");
+function removeAllActiveClasses(button) {
+  button.parentNode
+    .querySelectorAll("button")
+    .forEach((button) => button.classList.remove("active"));
+  button.classList.add("active");
+}
+
+document
+  .querySelectorAll(".origin-options.primary button")
+  .forEach((button) => {
+    const city = button.getAttribute("data-city");
+
+    button.addEventListener("click", function (e) {
+      removeAllActiveClasses(button);
+      const coordinates = citiesCoordinates[city]; // M coordinates
+      document.getElementById("coordinates").value = coordinates.join(",");
+    });
   });
-});
 
 document
   .querySelectorAll(".origin-options.secondary button")
-  .forEach(button => {
+  .forEach((button) => {
     const city = button.getAttribute("data-city");
     button.addEventListener("click", function (e) {
-      const coordinates = citiesCoordinates[city]; // Madrid coordinates
+      removeAllActiveClasses(button);
+      const coordinates = citiesCoordinates[city]; // M coordinates
       document.getElementById("coordinates-secondary").value =
         coordinates.join(",");
     });
